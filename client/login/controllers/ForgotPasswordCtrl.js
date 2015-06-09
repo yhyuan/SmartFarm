bdec54e01a221ece319845af1612d751ba0eb7b9
Jerry Yuan <yhyuan@gmail.com>
2015-06-09 14:57:40 -0400

    forgot password

--- client/login/controllers/ForgotPasswordCtrl.js
+++ client/login/controllers/ForgotPasswordCtrl.js
@@ -1,9 +1,29 @@
- angular.module('app.example').controller('ForgotPasswordCtrl', ['$scope', '$meteorCollection', '$ionicModal', '$rootScope', '$ionicSideMenuDelegate', '$ionicPopup', '$cordovaDatePicker',
-      function($scope, $meteorCollection, $ionicModal, $rootScope, $ionicSideMenuDelegate, $ionicPopup, $cordovaDatePicker) {
-		$scope.data = {};
- 
-	    $scope.forgotPassword = function() {
-	        console.log("LOGIN user: " + $scope.data.username + " - PW: " + $scope.data.password);
-	    }
-      }
-  ]);
+ angular.module('app.example').controller('ForgotPasswordCtrl', ['$scope', '$state', '$meteorCollection', '$ionicModal', '$rootScope', '$ionicSideMenuDelegate', '$ionicPopup', '$cordovaDatePicker',
+     function($scope, $state, $meteorCollection, $ionicModal, $rootScope, $ionicSideMenuDelegate, $ionicPopup, $cordovaDatePicker) {
+         $scope.data = {};
+         var forgotPasswordCallback = function() {
+             $state.go('login');
+         };
+         var forgotPasswordErrorback = function() {
+             var alertPopup = $ionicPopup.alert({
+                 title: 'Failed to send email',
+                 template: 'Failed to send email'
+             });
+             alertPopup.then(function(res) {
+                 //console.log('Thank you for not eating my delicious ice cream cone');
+             });
+         };
+
+         $scope.forgotPassword = function() {
+             Accounts.forgotPassword({
+                 email: $scope.data.username
+             }, function(err) {
+                 if (err)
+                    forgotPasswordErrorback();
+                 else {
+                    forgotPasswordCallback();
+                 }
+             });
+         }
+     }
+ ]);
--- client/login/controllers/SingupCtrl.js
+++ client/login/controllers/SingupCtrl.js
@@ -3,13 +3,47 @@
          $scope.data = {};
 
          $scope.signup = function() {
+             var trimInput = function(value) {
+                 return value.replace(/^\s*|\s*$/g, '');
+             };
+
+             var isNotEmpty = function(value) {
+                 if (value && value !== '') {
+                     return true;
+                 }
+                 return false;
+             };
+
+             var isEmail = function(value) {
+                 var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+                 if (filter.test(value)) {
+                     return true;
+                 }
+                 return false;
+             };
+
+             var isValidPassword = function(password) {
+                 if (password.length < 6) {
+                     //console.log('Your password should be 6 characters or longer.');
+                     return false;
+                 }
+                 return true;
+             };
+
+             var areValidPasswords = function(password, confirm) {
+                 if (password !== confirm) {
+                     //console.log('Your two passwords are not equivalent.');
+                     return false;
+                 }
+                 return true;
+             };
              var signupCallback = function() {
                  $state.go('login');
              };
-             var signupErrorback = function() {
+             var signupErrorback = function(msg) {
                  var alertPopup = $ionicPopup.alert({
                      title: 'Failed to sign up',
-                     template: 'Your fields are missing or the passwords do not match.'
+                     template: msg
                  });
                  alertPopup.then(function(res) {
                      //console.log('Thank you for not eating my delicious ice cream cone');
@@ -19,19 +53,35 @@
              var name = $scope.data.name;
              var password = $scope.data.password;
              var confirmedpassword = $scope.data.confirmedpassword;
-             if (username && name && password && confirmedpassword && (username.length > 0) && (name.length > 0) && (password.length > 0) && (confirmedpassword.length > 0) && (password === confirmedpassword)) {
-                 Accounts.createUser({
-                     email: username,
-                     password: password
-                 }, function(err) {
-                     if (err) {
-                         signupErrorback();
+             if (isNotEmpty(username) && isNotEmpty(name) && isNotEmpty(password) && isNotEmpty(confirmedpassword)) {
+                 username = trimInput(username);
+                 name = trimInput(name);
+                 password = trimInput(password);
+                 confirmedpassword = trimInput(confirmedpassword);
+                 if (isEmail(username)) {
+                     if (isValidPassword(password) && isValidPassword(confirmedpassword)) {
+                         if (areValidPasswords(password, confirmedpassword)) {
+                             Accounts.createUser({
+                                 email: username,
+                                 password: password
+                             }, function(err) {
+                                 if (err) {
+                                     signupErrorback(err);
+                                 } else {
+                                     signupCallback();
+                                 }
+                             });
+                         } else {
+                             signupErrorback('Your two passwords are not equivalent.');
+                         }
                      } else {
-                         signupCallback();
+                         signupErrorback("Passowrd is not valid.");
                      }
-                 }); //
+                 } else {
+                     signupErrorback("Email is not valid.");
+                 }
              } else {
-                 signupErrorback();
+                 signupErrorback("Filed are empty.");
              }
          }
      }
